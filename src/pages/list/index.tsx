87e14ebb689d99791f9202a2868846361bb3e5f0
Walter Nascimento Barroso <walter.barroso@mobly.com.br>
2021-03-03 15:00:53 -0400

    [ADJUST] New changes for list

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  List,
-  ListItem,
-  ListItemSecondaryAction,
-  ListItemText,
-  ListItemAvatar,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Button,
   Checkbox,
+  FormControlLabel,
+  Typography,
+  Divider,
+  ListItemAvatar,
   Avatar,
   Paper,
   Container,
@@ -14,6 +16,10 @@ import {
   Tooltip,
 } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import ImageIcon from '@material-ui/icons/Image';
 import LayoutWithMenu from '../../components/layout/LayoutWithMenu/LayoutWithMenu';
 
 const useStyles = makeStyles((theme) => ({
@@ -22,53 +28,90 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
+  divider: {
+    border: `1px solid ${theme.palette.divider}`,
+    margin: '0 1rem',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '66.66%',
+    flexShrink: 0,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+    display: 'flex',
+    alignItems: 'center',
+  },
 }));
 export default function CheckboxListSecondary() {
-  const [checked, setChecked] = React.useState([1]);
-
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
   const classes = useStyles();
 
   return (
     <LayoutWithMenu>
       <Container>
         <Paper elevation={3}>
-          <List dense>
-            {[0, 1, 2, 3].map((value) => {
-              const labelId = `checkbox-list-secondary-label-${value}`;
-              return (
-                <ListItem key={value} button>
-                  <ListItemAvatar>
-                    <Avatar alt={`Avatar n°${value + 1}`}>{value}</Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    id={labelId}
-                    primary={`Line item ${value + 1}`}
+          {[0, 1, 2, 3].map((value) => {
+            return (
+              <Accordion key={value}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-label="Expand"
+                  aria-controls={`additional-${value}-content`}
+                  id={`additional-${value}-header`}
+                >
+                  <FormControlLabel
+                    aria-label="Acknowledge"
+                    onClick={(event) => event.stopPropagation()}
+                    onFocus={(event) => event.stopPropagation()}
+                    control={<Checkbox />}
+                    label=""
                   />
-                  <ListItemSecondaryAction>
-                    <Checkbox
-                      edge="end"
-                      onChange={handleToggle(value)}
-                      checked={checked.indexOf(value) !== -1}
-                      inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                  </ListItemSecondaryAction>
-                </ListItem>
-              );
-            })}
-          </List>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <ImageIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <Typography className={classes.heading}>Sal</Typography>
+                  <Typography className={classes.secondaryHeading}>
+                    3 UN
+                  </Typography>
+                  <Divider
+                    orientation="vertical"
+                    flexItem
+                    className={classes.divider}
+                  />
+                  <Typography className={classes.secondaryHeading}>
+                    R$ 5,99
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography color="textSecondary">
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      startIcon={<EditIcon />}
+                    >
+                      Editar
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<DeleteIcon />}
+                    >
+                      Remover
+                    </Button>
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+        </Paper>
+
+        <Paper elevation={3} square>
+          Total
         </Paper>
       </Container>
 
